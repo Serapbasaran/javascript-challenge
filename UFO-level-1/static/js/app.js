1ae80eb3aae513c776c5c3d4edd9a02dc1dcbcd9
Serapbasaran <serap.cbasaran@gmail.com>
2020-12-19 22:56:49 -0500

    Update app.js

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,15 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// Get a reference to the table body
+var tbody = d3.select("tbody");
+
+
+tableData.forEach((sightingdata) => {
+    var row = tbody.append("tr");
+    Object.entries(sightingdata).forEach(([key, value]) => {
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  });
+  
