574a68b934b78e69c17197a084c18b2b3cc34518
Serapbasaran <serap.cbasaran@gmail.com>
2020-12-22 14:17:11 -0500

    Update app.js

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -5,11 +5,20 @@ var tableData = data;
 var tbody = d3.select("tbody");
 
 
-tableData.forEach((sightingdata) => {
-    var row = tbody.append("tr");
-    Object.entries(sightingdata).forEach(([key, value]) => {
-      var cell = row.append("td");
-      cell.text(value);
-    });
-  });
-  
+// Console.log  data from data.js
+console.log(tableData);
+
+
+// // Step 5: Use d3 to update each cell's text with tableData values 
+tableData.forEach(function(sightingdata) {
+console.log(sightingdata);
+var row = tbody.append("tr");
+Object.entries(sightingdata).forEach(function([key, value]) {
+ console.log(key, value);
+//// Append a cell to the row for each value
+
+ var cell = row.append("td");
+ cell.text(value);
+});
+});
+
