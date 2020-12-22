d56947d069182ec58060ba06b3d05f20bb2ec3e3
Serapbasaran <serap.cbasaran@gmail.com>
2020-12-22 18:43:08 -0500

    Update app.js

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -1,4 +1,49 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// Get a reference to the table body
+var tbody = d3.select("tbody");
+
+
+// Console.log  data from data.js
+console.log(tableData);
+
+
+//  Use d3 to update each cell's text with tableData values 
+tableData.forEach((weatherReport) => {
+    var row = tbody.append("tr");
+    Object.entries(weatherReport).forEach(([key, value]) => {
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  });
+  
+
+// Select the button
+var button = d3.select("#button");
+
+// Select the form
+var form = d3.select("#form");
+
+// Create event handlers 
+//button.on("click", runEnter);
+//form.on("submit",runEnter);
+
+// Complete the event handler function for the form
+//function runEnter() {
+
+  // Prevent the page from refreshing
+  //d3.event.preventDefault();
+  
+  // Select the input element and get the raw HTML node
+  var inputElement = d3.select("#datetime.form-control");
+
+  // Get the value property of the input element
+  var inputValue = inputElement.property("value");
+
+  console.log(inputValue);
+  
+
+  var filteredData = tableData.filter(person => person.datetime === inputValue);
+
+  console.log(filteredData);
